84f21e3d5e25f90c767a8430f10353a5c49ea80b
Protector20 <dima.burkin96@yandex.ru>
2021-10-04 21:45:31 +0300

    Update task 51.sql

--- SQL tasks/task 51.sql
+++ SQL tasks/task 51.sql
@@ -3,6 +3,31 @@
 */
 
 
-
+SELECT name
+FROM (SELECT O.ship AS name, numGuns, displacement
+FROM Outcomes O INNER JOIN
+Classes C ON O.ship = C.class AND
+O.ship NOT IN (SELECT name
+FROM Ships
+)
+UNION
+SELECT S.name AS name, numGuns, displacement
+FROM Ships S INNER JOIN
+Classes C ON S.class = C.class
+) OS INNER JOIN
+(SELECT MAX(numGuns) AS MaxNumGuns, displacement
+FROM Outcomes O INNER JOIN
+Classes C ON O.ship = C.class AND
+O.ship NOT IN (SELECT name
+FROM Ships
+)
+GROUP BY displacement
+UNION
+SELECT MAX(numGuns) AS MaxNumGuns, displacement
+FROM Ships S INNER JOIN
+Classes C ON S.class = C.class
+GROUP BY displacement
+) GD ON OS.numGuns = GD.MaxNumGuns AND
+OS.displacement = GD.displacement
 
 
